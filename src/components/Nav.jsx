19cf886ba4fcccd3c382e8ccddba90971141c9fe
Chris Scott <cam022007@coderacademy.edu.au>
2020-12-15 04:17:51 -0800

    nav rendering and a main page + a nav page set up for linking

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav>  
+      <a href="/">Home</a>
+      <a href="/">Choose your Pokemon!</a>
+    </nav>
+  )
+}
+
+export default Nav;
